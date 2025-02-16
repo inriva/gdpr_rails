e2b46a1e8f2d8624d0240c8805e6a820f06636e6
jfr <ritscher@inriva.com>
2025-02-16 16:38:47 +0100

    Update engine.rb
    patch for rails 7

--- lib/policy_manager/engine.rb
+++ lib/policy_manager/engine.rb
@@ -3,13 +3,13 @@ module PolicyManager
     isolate_namespace PolicyManager
     config.autoload_paths << File.expand_path("lib/generators", __dir__)
     config.generators do |g|
-      g.test_framework :rspec, :fixture => false
-      #g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+      g.test_framework :rspec, fixture: false
       g.assets false
       g.helper false
     end
 
-    config.assets.precompile += %w( policy_manager/application.css policy_manager/application.js)
-
+    if config.respond_to?(:assets)
+      config.assets.precompile += %w( policy_manager/application.css policy_manager/application.js )
+    end
   end
 end
